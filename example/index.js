51ec8f59e32e8c2bb95d134d6a72e2127887a67b
Jérémie Zarca <jeremie.zarca@gmail.com>
2019-08-28 20:05:14 +0200

    added yarn.lock

--- example/index.js
+++ example/index.js
@@ -8,22 +8,19 @@ async function init() {
   try {
     await franprix.signIn();
     const infos = await franprix.getMyInfos();
-
+    
     const promotions = await franprix.getAvailablePromotions();
-    const advantages = await franprix.getAvailableAdvantages();
 
     const allOffersIds = promotions
-      .concat(advantages)
       .map(offer => offer.id.toString());
 
     if (allOffersIds.length) {
       await franprix.addOffersToCard(allOffersIds);
     }
-
+    
     const myPromotions = await franprix.getMyPromotions();
-    const myAdvantages = await franprix.getMyAdvantages();
 
-    console.log(myAdvantages);
+    console.log(myPromotions.map(acc => acc.name, []).join('\n'));
   } catch (err) {
     console.log(err);
   }
